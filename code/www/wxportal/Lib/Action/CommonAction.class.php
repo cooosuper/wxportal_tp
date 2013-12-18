3657a4c591ab3f84cbe8636dc75f710147313899
cooosuper <cooosuper@gmail.com>
2013-12-18 04:35:44 -0800

    重构框架

---
+++ code/www/wxportal/Lib/Action/CommonAction.class.php
@@ -0,0 +1,11 @@
+<?php
+class CommonAction extends Action{
+    //调用所有方法时都会触发此方法
+    public function _initialize(){
+        /*
+        if(!isset($_SESSION['uid'])){
+            $this->redirect(GROUP_NAME . '/Login/index');
+        }*/
+    }
+}
+?>
---
+++ code/www/wxportal/Modules/C/Action/IndexAction.class.php
@@ -0,0 +1,19 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class IndexAction extends CommonAction {
+    public function index(){
+        $this->display();
+        die;
+        echo 'This is C index';
+        $tb_user = M('user'); // 实例化user数据模型
+        $this->data_user = $tb_user->select();
+        $this->display();
+        die;
+        $this->name = 'thinkphp'; // 进行模板变量赋值
+        $this->display();
+    }
+
+    public function subpage(){
+        $this->display();
+    }
+}
