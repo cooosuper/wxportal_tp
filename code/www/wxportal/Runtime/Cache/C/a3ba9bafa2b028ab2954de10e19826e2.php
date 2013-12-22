4e17ba8a827f481da7c430c67e2862d10e191215
cooosuper <cooosuper@gmail.com>
2013-12-22 08:56:56 -0800

    图片地址-->跳转地址

--- code/www/wxportal/Runtime/Cache/C/a3ba9bafa2b028ab2954de10e19826e2.php
+++ code/www/wxportal/Runtime/Cache/C/a3ba9bafa2b028ab2954de10e19826e2.php
@@ -131,7 +131,7 @@
 							<td style="background-color: silver">标题</td>
 							<td style="background-color: silver">简介</td>
 							<td style="background-color: silver">配图</td>
-							<td style="background-color: silver">图片地址</td>
+							<td style="background-color: silver">跳转地址</td>
 							<td style="background-color: silver">操作</td>
 						</tr>
 						<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><tr>
